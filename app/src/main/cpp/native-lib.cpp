4d1a188e25205e52ca31831ee9641a2219cf6e78
boo <jaesik.boo@2beone.net>
2023-08-25 19:00:33 +0900

    Finish oboe default function

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,54 +7,15 @@
 #include <android/asset_manager.h>
 #include <oboe/Oboe.h>
 #include <android/log.h>
+#include "AudioPlayer.h"
+#include "utils/logging.h"
 
 
-#define LOGE(...)
-#define LOGI(...)
-
-/*extern "C" JNIEXPORT jstring JNICALL
-Java_com_boo_sample_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject  thiz ) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}*/
+//#define LOGE(...)
+//#define LOGI(...)
 
 //Oboe
-oboe::AudioStreamBuilder builder;
-
-class AudioDataCallback : public oboe::AudioStreamDataCallback {
-    public:
-        oboe::DataCallbackResult
-        onAudioReady(oboe::AudioStream *audioStream, void *audioData, int32_t numFrames) {
-            //We requested AudioFormat::Float. So if the stream opens we know we got the Float format
-            // If you do not specify a format then you should check what format
-            // the stream has and cast to the appropriate type.
-            auto *outputData = static_cast<float *>(audioData);
-
-            //Generate random numbers (white noise) centered around zero.
-            const float amplitude = 0.2f;
-            for (int i = 0; i < numFrames; ++i) {
-                outputData[i] = ((float) drand48() - 0.5f) * 2 * amplitude;
-            }
-            return oboe::DataCallbackResult::Continue;
-        }
-};
-
-std::shared_ptr<oboe::AudioStream> mStream;
-
-struct AudioProperties {
-    int32_t channelCount;
-    int32_t sampleRate;
-};
-
-//bool setUpAudioSource() {
-//    AudioProperties targetProperties {
-//        .channelCount = 2,
-//        .sampleRate = 48000
-//    };
-
-//}
+std::unique_ptr<AudioPlayer> audioPlayer;
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -65,28 +26,24 @@ Java_com_boo_sample_oboe_OboeUtilKt_stringFromJNI(JNIEnv *env, jclass clazz) {
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_boo_sample_oboe_OboeUtilKt_oboeStart(JNIEnv *env, jclass clazz, jobject asset_manager) {
-//    oboe::Result result = builder.openStream(mStream);
-//
-//    if(result != oboe::Result::OK) {
-//        LOGE("Failed to create stream. Error: %s", oboe::convertToText(result))
-//    }
-//
-//    oboe::AudioFormat format = mStream->getFormat();
-//    LOGI("AudioStream format is %s", oboe::convertToText(format))
-//
-//    mStream->requestStart();
+Java_com_boo_sample_oboe_OboeUtilKt_oboeStart(JNIEnv *env, jclass clazz, jobject jAssetManager) {
+    LOGD("oboeStart");
+
+    AAssetManager *assetManager = AAssetManager_fromJava(env, jAssetManager);
+    if (assetManager == nullptr) {
+        LOGE("Could not obtain the AAssetManager");
+        return;
+    }
+
+    audioPlayer = std::make_unique<AudioPlayer>(*assetManager);
+    audioPlayer->start();
 
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_boo_sample_oboe_OboeUtilKt_oboeStop(JNIEnv *env, jclass clazz) {
-    if(mStream) {
-        mStream->stop();
-        mStream->close();
-        mStream.reset();
-    }
+    audioPlayer->stop();
 }
 
 extern "C"
@@ -94,14 +51,8 @@ JNIEXPORT void JNICALL
 Java_com_boo_sample_oboe_OboeUtilKt_oboeSetDefaultStreamValue(JNIEnv *env, jclass clazz,
                                                               jint sampleRate,
                                                               jint framesPerBurst) {
+    LOGD("oboeSetDefaultStreamValue");
+
     oboe::DefaultStreamValues::SampleRate = (int32_t) sampleRate;
     oboe::DefaultStreamValues::FramesPerBurst = (int32_t) framesPerBurst;
-
-    AudioDataCallback dataCallback;
-
-    builder.setPerformanceMode(oboe::PerformanceMode::LowLatency);
-    builder.setSharingMode(oboe::SharingMode::Exclusive);
-    builder.setDataCallback(&dataCallback);
-    builder.setFormat(oboe::AudioFormat::Float);
-    builder.setChannelCount(oboe::ChannelCount::Mono);
 }
--- app/src/main/java/com/boo/sample/activities/MainActivity.kt
+++ app/src/main/java/com/boo/sample/activities/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.boo.sample.activities
 
+import android.content.Context
+import android.content.res.AssetManager
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import com.boo.sample.base.BaseActivity
 import com.boo.sample.main.R
 import com.boo.sample.main.databinding.ActivityMainBinding
 import com.boo.sample.oboe.OboeUtil
+import com.boo.sample.oboe.oboeSetDefaultStreamValue
+import com.boo.sample.oboe.oboeStart
+import com.boo.sample.oboe.oboeStop
 import com.boo.sample.oboe.stringFromJNI
 import com.boo.sample.repository.preference.PreferenceRepository
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,8 +31,28 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
         System.loadLibrary("native-lib")
 
-        binding.testView.text = stringFromJNI()
-        Log.d(TAG, "I am main")
-        Log.d(TAG, "JNI call : ${stringFromJNI()}")
+        val myAudioMgr = baseContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val sampleRateStr = myAudioMgr.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE)
+        val defaultSampleRate = Integer.parseInt(sampleRateStr)
+        val framesPerBurstStr = myAudioMgr.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER)
+        val defaultFramesPerBurst = Integer.parseInt(framesPerBurstStr)
+
+        oboeSetDefaultStreamValue(defaultSampleRate, defaultFramesPerBurst)
+
+        findViewById<Button>(R.id.startBtn).setOnClickListener {
+            Log.d(TAG, "startBtn click")
+
+            oboeStart(assets)
+        }
+
+        findViewById<Button>(R.id.stopBtn).setOnClickListener {
+            Log.d(TAG, "stopBtn click")
+
+            oboeStop()
+        }
+//        binding.testView.text = stringFromJNI()
+//        Log.d(TAG, "I am main")
+//        Log.d(TAG, "JNI call : ${stringFromJNI()}")
     }
+
 }
